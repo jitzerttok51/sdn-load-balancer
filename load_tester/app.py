2061680d92e5a908e24e76571209f596613009ae
Prodanov, Nikolay <jitzerttok98@gmail.com>
2021-09-12 11:30:51 +0300

    Added monitor capability

--- load_tester/app.py
+++ load_tester/app.py
@@ -1,18 +1,60 @@
-from flask import Flask
+from flask import Flask, request
 import os
+import threading
+import time
+import socket
 
 app = Flask(__name__)
 
 counter = 0
 host = os.environ['HOST']
+monitorIp = os.environ['MONITOR_IP']
+monitorPort = int(os.environ['MONITOR_PORT'])
+
+load = {}
 
 @app.route("/")
 def home():
+    count()
+    return f"{host}: {counter}" 
+
+@app.route("/load")
+@app.route("/load/<magnitude>")
+@app.route('/load/<magnitude>/<seconds>')
+def addLoad(magnitude="1", seconds="1"):
+    global load
+    reqId = request.__hash__()
+    load[reqId] = int(magnitude)
+    time.sleep(int(seconds))
+    if reqId in load:
+        del load[reqId]
+    return magnitude
+
+def count():
     global counter
     counter = counter + 1
     save(host, counter)
-    return f"{host}: {counter}" 
 
 def save(host, count):
     with open(f"{host}.txt", 'w') as f:
         f.write(str(count))
+
+
+def submitLoad():
+    hostLoad = sum(load.values())
+    print(f"Current load: {hostLoad}")
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(bytes([hostLoad]), (monitorIp, monitorPort))
+
+def createTimer(target, seconds):
+    def iter():
+        while True:
+            target()
+            time.sleep(seconds)
+    th = threading.Thread(target=iter, daemon=True)
+    th.start()
+    return th
+
+if __name__ == "__main__":
+    createTimer(submitLoad, 2)
+    app.run(host="0.0.0.0", debug=False)
